799c16c107f15cb6846d8b1e99c61344799c8e43
tobinhuang <tobinhuang@tencent.com>
2021-03-03 23:07:39 +0800

    117. 填充每个节点的下一个右侧节点指针 II

---
+++ chap9-树/117. 填充每个节点的下一个右侧节点指针 II/connect.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if root == None:
+            return root
+
+        queue = [root]
+        while len(queue) > 0:
+            curLength = len(queue)
+            nextNode = []
+            for index in range(curLength):
+                if queue[index].left != None:
+                    nextNode.append(queue[index].left)
+                if queue[index].right != None:
+                    nextNode.append(queue[index].right)
+                if index < curLength - 1:
+                    queue[index].next = queue[index + 1]
+            if len(nextNode) > 0:
+                queue = nextNode
+            else:
+                break
+        return root
