73086d04432b11d12a4cd3f5c7a5d6e2a2e51f89
tobinhuang <jwei_huang@foxmail.com>
2021-03-04 22:58:39 +0800

    113. 路径总和 II

---
+++ chap9-树/113. 路径总和 II/pathSum.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+import copy
+class Solution:
+    def __init__(self):
+        self.arr=[]
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if root==None:
+            return []
+        cur=[]
+        self.node_sum(root,cur,targetSum)
+        return self.arr
+
+    def node_sum(self,root,cur,targetSum):
+        if root==None:
+            return
+        cur.append(root.val)
+        if root.left==None and root.right==None:
+            if  sum(cur)==targetSum:
+                self.arr.append(copy.copy(cur))
+        self.node_sum(root.left,cur,targetSum)
+        self.node_sum(root.right,cur,targetSum)
+        cur.pop()
+        return 
